f8bfced57689a3252059dae3fea2d3d79c15862f
Hakkelo89 <78032311+Hakkelo89@users.noreply.github.com>
2021-08-06 19:02:14 +0100

    app.js refactored

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
-
+import { ApolloProvider } from "@apollo/react-hooks";
+import ApolloClient from "apollo-boost";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import {
-  ApolloClient,
-  ApolloProvider,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client";
-import { setContext } from "@apollo/client/link/context";
 import Navbar from "./components/Navbar";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 
-/* const client = new ApolloClient({
+const client = new ApolloClient({
   request: (operation) => {
     const token = localStorage.getItem("id_token");
 
@@ -23,26 +17,6 @@ import SavedBooks from "./pages/SavedBooks";
     });
   },
   uri: "/graphql",
-}); */
-
-const httpLink = createHttpLink({
-  uri: process.env.GRAPHQL_URL || "http://localhost:3001/graphql",
-});
-
-const authLink = setContext((_, { headers }) => {
-  const user = JSON.parse(localStorage.getItem("user"));
-
-  return {
-    headers: {
-      ...headers,
-      authorization: user ? `Bearer ${user.token}` : "",
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
 });
 
 function App() {
--- server/server.js
+++ server/server.js
@@ -15,19 +15,19 @@ const server = new ApolloServer({
   context: authMiddleware,
 });
 
-const startServer = async () => {
-  await server.start();
-  server.applyMiddleware({ app });
-};
-
-startServer();
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../client/build")));
 }
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+});
 
 db.once("open", () => {
   app.listen(PORT, () => {
